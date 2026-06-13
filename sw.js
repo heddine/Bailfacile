38dbd1135d0c8141a2e4ca9704b68916435479fe
heddine <heddineveline@gmail.com>
2026-06-13 14:07:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bailfacile-v5';
+const CACHE_NAME = 'bailfacile-v6';
 const PRECACHE_URLS = ['index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(PRECACHE_URLS)).then(() => self.skipWaiting()));
